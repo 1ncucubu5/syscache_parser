8276478eb2135df8801c0a1652d9afc9b760cbed
dodg1444 <dodg.mee@mail.ru>
2022-03-01 21:07:06 +0300

    Minor hve parser refactoring

--- hve_to_csv/parser.py
+++ hve_to_csv/parser.py
@@ -10,14 +10,15 @@ args = parser.parse_args()
 f = open(args.outfile, 'a')
 reg = Registry.Registry(args.inputfile)
 
+
 def rec(key, depth=0):
-	for subkey in key.subkeys():
-		rec(subkey, depth + 0)
-		for value in [v for v in key.values() 
-				if v.value_type() == Registry.RegBin]:
-					shorten = value.value()
-					shorten = shorten.decode('utf-16').rstrip('\x00')
-					sha1 = shorten[4:]
+    for subkey in key.subkeys():
+        rec(subkey, depth + 0)
+        for value in [v for v in key.values()
+                      if v.value_type() == Registry.RegBin]:
+            shorten = value.value()
+            shorten = shorten.decode('utf-16').rstrip('\x00')
+
 
 rec(reg.root())
 f.close()
